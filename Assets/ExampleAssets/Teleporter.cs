41e87cc86d7641cea3a6e7054c78a9dfb2d78ea1
mkhadadeh <mkhadadeh@scu.edu>
2020-05-29 11:47:39 -0700

    Initial commit

---
+++ Assets/ExampleAssets/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    const float multiple = 127.3f;
+    public Transform[] cylinders;
+    public Transform player;
+    int currentRing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRing = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.y < cylinders[currentRing].position.y - 2 * multiple)
+        {
+            cylinders[currentRing].position -= new Vector3(0, 5 * multiple, 0);
+            currentRing = (currentRing + 1) % 5;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# XRInputPlayerController
+ A PlayerController for Unity 2019.3 that uses Unity's built in XRInput
